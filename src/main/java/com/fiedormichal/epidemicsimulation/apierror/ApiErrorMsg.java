aab45b819e7e5862a7c29bc7c96bec762fd0f2fa
MichalFiedor <michal.fiedor92@gmail.com>
2021-03-06 21:52:03 +0100

    Exception handlers done.

--- src/main/java/com/fiedormichal/epidemicsimulation/apierror/ApiErrorMsg.java
+++ src/main/java/com/fiedormichal/epidemicsimulation/apierror/ApiErrorMsg.java
@@ -6,6 +6,11 @@ import lombok.Getter;
 @AllArgsConstructor
 @Getter
 public enum ApiErrorMsg {
+
+    ERROR_OCCURRED("Some exceptions occurred. Check errors list."),
+    METHOD_NOT_FOUND("Method with this URL not found."),
+    UNSUPPORTED_MEDIA_TYPE("Specified request media type (Content type) is not supported. Got to errors list for details."),
+    MISMATCH_TYPE("Wrong method parameter in URL."),
     VALIDATION_ERRORS("Occurred some validation errors. Please check if JSON contains correct values. "
             + "Check errors list for details."),
     VIOLATION_ERRORS("Occurred some violation errors. Please check if JSON contains correct values. "
--- src/main/java/com/fiedormichal/epidemicsimulation/exception/RestExceptionHandler.java
+++ src/main/java/com/fiedormichal/epidemicsimulation/exception/RestExceptionHandler.java
@@ -2,20 +2,19 @@ package com.fiedormichal.epidemicsimulation.exception;
 
 import com.fiedormichal.epidemicsimulation.apierror.ApiError;
 import com.fiedormichal.epidemicsimulation.apierror.ApiErrorMsg;
-import javassist.NotFoundException;
-import org.springframework.beans.TypeMismatchException;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.HttpRequestMethodNotSupportedException;
+import org.springframework.web.HttpMediaTypeNotAcceptableException;
+import org.springframework.web.HttpMediaTypeNotSupportedException;
 import org.springframework.web.bind.MethodArgumentNotValidException;
-import org.springframework.web.bind.MissingPathVariableException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 import org.springframework.web.context.request.WebRequest;
+import org.springframework.web.method.annotation.MethodArgumentTypeMismatchException;
+import org.springframework.web.servlet.NoHandlerFoundException;
 import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
 
-import javax.validation.ConstraintViolationException;
 import java.time.LocalDateTime;
 import java.util.ArrayList;
 import java.util.List;
@@ -23,6 +22,32 @@ import java.util.stream.Collectors;
 
 @RestControllerAdvice
 public class RestExceptionHandler extends ResponseEntityExceptionHandler {
+
+    @Override
+    protected ResponseEntity<Object> handleNoHandlerFoundException(NoHandlerFoundException ex,
+                                                                   HttpHeaders headers,
+                                                                   HttpStatus status,
+                                                                   WebRequest request) {
+        List<String> errors = new ArrayList<>();
+        errors.add(String.format("Could not find the %s method for URL %s",
+                ex.getHttpMethod(), ex.getRequestURL()));
+        return buildResponseEntity(getApiError(HttpStatus.NOT_FOUND, errors, ApiErrorMsg.METHOD_NOT_FOUND.getValue()));
+    }
+
+    @Override
+    protected ResponseEntity<Object> handleHttpMediaTypeNotSupported(HttpMediaTypeNotSupportedException ex,
+                                                                     HttpHeaders headers,
+                                                                     HttpStatus status,
+                                                                     WebRequest request) {
+        List<String> errors = new ArrayList<>();
+        StringBuilder builder = new StringBuilder();
+        builder.append(ex.getContentType());
+        builder.append(" media type is not supported. Supported media types are ");
+        ex.getSupportedMediaTypes().forEach(type -> builder.append(type).append(", "));
+        errors.add(builder.toString());
+
+        return buildResponseEntity(getApiError(HttpStatus.UNSUPPORTED_MEDIA_TYPE, errors, ApiErrorMsg.UNSUPPORTED_MEDIA_TYPE.getValue()));
+    }
 
     @Override
     protected ResponseEntity<Object> handleMethodArgumentNotValid(MethodArgumentNotValidException ex,
@@ -45,6 +70,22 @@ public class RestExceptionHandler extends ResponseEntityExceptionHandler {
         errors.add(ex.getMessage());
         return buildResponseEntity(getApiError(HttpStatus.NOT_FOUND, errors, ApiErrorMsg.INITIAL_DATA_NOT_FOUND.getValue()));
     }
+
+    @ExceptionHandler(MethodArgumentTypeMismatchException.class)
+    protected ResponseEntity<Object> handleMethodArgumentMismatch(Exception ex){
+        List<String>errors = new ArrayList<>();
+        errors.add(ex.getMessage());
+        return buildResponseEntity(getApiError(HttpStatus.BAD_REQUEST, errors, ApiErrorMsg.MISMATCH_TYPE.getValue()));
+    }
+
+    @ExceptionHandler(Exception.class)
+    protected ResponseEntity<?> handleAll(Exception ex) {
+        List<String> errors = new ArrayList<>();
+        errors.add(ex.getLocalizedMessage());
+
+        return buildResponseEntity(getApiError(HttpStatus.BAD_REQUEST,errors, ApiErrorMsg.ERROR_OCCURRED.getValue()));
+    }
+
 
     private ResponseEntity<Object> buildResponseEntity(ApiError apiError){
         return new ResponseEntity<>(apiError, apiError.getStatus());
