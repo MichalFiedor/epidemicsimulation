11ca44169a8787a0093b23babda12f6c1458ac6c
MichalFiedor <michal.fiedor92@gmail.com>
2021-03-21 22:33:50 +0100

    changes in properties.

--- src/main/java/com/fiedormichal/epidemicsimulation/EpidemicSimulationApplication.java
+++ src/main/java/com/fiedormichal/epidemicsimulation/EpidemicSimulationApplication.java
@@ -1,10 +1,11 @@
 package com.fiedormichal.epidemicsimulation;
 
+import com.ulisesbocchio.jasyptspringboot.annotation.EnableEncryptableProperties;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 
 @SpringBootApplication
+@EnableEncryptableProperties
 public class EpidemicSimulationApplication {
 
     public static void main(String[] args) {
