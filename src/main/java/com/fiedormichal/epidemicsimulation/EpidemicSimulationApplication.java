80a5c0325c0414fc9d39196c7df89c1566c9302f
MichalFiedor <michal.fiedor92@gmail.com>
2021-03-22 18:29:39 +0100

    changes in properties.

--- src/main/java/com/fiedormichal/epidemicsimulation/EpidemicSimulationApplication.java
+++ src/main/java/com/fiedormichal/epidemicsimulation/EpidemicSimulationApplication.java
@@ -5,9 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class EpidemicSimulationApplication {
-
     public static void main(String[] args) {
         SpringApplication.run(EpidemicSimulationApplication.class, args);
     }
-
 }
